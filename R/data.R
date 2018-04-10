2574401b02a69882976dea0661917511f9dfca77
James Blair <james.m.blair09@gmail.com>
2018-04-10 01:47:39 -0600

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,26 @@
+#' GPA data
+#'
+#' A dataset containing SAT score, High School GPA, and college GPA for 1000
+#' students.
+#'
+#' @format a data frame with 1000 rows and 3 variables
+#' \describe{
+#'   \item{sat}{SAT scores}
+#'   \item{hsgpa}{High School GPA}
+#'   \item{cgpa}{College GPA}
+#' }
+#' @source \url{https://dahl.byu.edu/223/2018a2/l12/gpa.txt}
+"gpa"
+
+#' Data about brothers' income
+#'
+#' A dataset containing income details about older and younger brothers.
+#'
+#' @format a data frame with 14 rows and 3 variables
+#' \describe{
+#'   \item{family}{identifier for the family}
+#'   \item{older}{income of older brother}
+#'   \item{younger}{income of younger brother}
+#' }
+#' @source \url{https://dahl.byu.edu/223/2018a2/l12/brothers.txt}
+"brothers"
